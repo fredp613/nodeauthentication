6613235794d8b73f6c259bb639cce9a99a5f514b
Frederick Pearson <fredp613@gmail.com>
2016-10-20 01:04:05 +0000

    fix flight plan

--- flightplan.js
+++ flightplan.js
@@ -56,7 +56,7 @@ plan.remote(function(remote) {
 
   remote.log('Reload application');
   remote.sudo('ln -snf ~/' + tmpDir + ' ~/'+appName, {user: username});
-  remote.sudo('cp .env /home/deploy/'+appName+'/.env'
+  remote.sudo('cp .env /home/deploy/'+appName+'/.env');
   remote.exec('sudo restart nodeauthentication');
 //  remote.exec('node /home/deploy/node-app/bin/index.js');
 //  remote.exec('forever stop ~/'+appName+'/'+startFile, {failsafe: true});
